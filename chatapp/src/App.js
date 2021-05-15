edbd51bb1e196e3cc5bef6aa7bfcd13eaaa172ea
rajeeva-netizen <74817212+rajeeva-netizen@users.noreply.github.com>
2021-05-15 16:17:32 +0530

    updated the app

--- chatapp/src/App.js
+++ chatapp/src/App.js
@@ -1,17 +1,19 @@
 import { ChatEngine } from 'react-chat-engine';
-import ChatFeed from './components/ChatFeed'
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 import './App.css';
 
 
 function App() {
+  if (!localStorage.getItem('username')) return <LoginForm />;
   return (
     <div className="App">
       {/* Chat application is coming */}
       <ChatEngine
       height='100vh'
       projectID='35b47058-5fd3-4333-88f9-a308aeb4502d'
-      userName='RajeevaBP'
-      userSecret='123456'
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
       renderChatFeed={(chatAppProps)=><ChatFeed {...(chatAppProps)}/>}
 
       />
